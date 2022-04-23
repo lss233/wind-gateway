75c69d31db47e9f07e129a48b32f737af09d4292
Lss233 <8984680+lss233@users.noreply.github.com>
2022-04-24 03:03:35 +0800

    Setting up Loggers.

--- src/main/java/com/lss233/wind/gateway/Application.java
+++ src/main/java/com/lss233/wind/gateway/Application.java
@@ -1,8 +1,18 @@
 package com.lss233.wind.gateway;
 
-public class Application {
-//    private static Logger
-    public static void main(String[] args) {
+import org.apache.logging.log4j.LogManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+public class Application {
+    private final static Logger LOG = LoggerFactory.getLogger(Application.class);
+    public static void main(String[] args) {
+        LoggerFactory.getLogger("GWBanner").info("Start blowing...\n" +
+                "\n" +
+                "m     m   \"               #           mmm m     m\n" +
+                "#  #  # mmm    m mm    mmm#         m\"   \"#  #  #\n" +
+                "\" #\"# #   #    #\"  #  #\" \"#         #   mm\" #\"# #\n" +
+                " ## ##\"   #    #   #  #   #         #    # ## ##\"\n" +
+                " #   #  mm#mm  #   #  \"#m##          \"mmm\" #   #\n");
     }
 }
